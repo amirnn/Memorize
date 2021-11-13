575f63208933b9e115d821e6b64ffb53f7bc7cd0
Amir Nourinia <amir.nourinia@gmail.com>
2021-11-13 05:22:49 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Amir Nourinia on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
